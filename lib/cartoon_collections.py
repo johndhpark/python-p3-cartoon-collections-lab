09f05d948d6a28dad813044c3f50a0601cdca31f
John DH. Park <john.dh.park@icloud.com>
2023-08-08 10:43:36 -0700

    Completed Cartoon Collections Lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, dwarf in enumerate(dwarves, start=1):
+        print(f"{index}. {dwarf}")
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(vegetables):
+    return [f"{vegetable.capitalize()}!" for vegetable in vegetables]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+
+    return False
+
+
+def find_the_cheese(foods):
+    if "cheddar" in foods:
+        return "cheddar"
+
+    return None
